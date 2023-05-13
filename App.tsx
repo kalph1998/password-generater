e2219f2e74f19de6127b3a9f8df6a9f46e88fff3
Kalph upadhyay <kalpupadhyay@gmail.com>
2023-05-13 18:58:46 +0530

    added logic

--- App.tsx
+++ App.tsx
@@ -1,21 +1,67 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import * as Yup from 'yup';
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import * as Yup from "yup";
 
 const PasswordSchema = Yup.object().shape({
   passwordLength: Yup.number()
     .required()
-    .min(4, 'should be min of 4 ')
-    .max(16, 'should be max of 16'),
+    .min(4, "should be min of 4 ")
+    .max(16, "should be max of 16"),
 });
 
 const App = () => {
-  const [password, setPassword] = useState('');
-  const [isPassGenerated, setisPassGenerated] = useState(false);
+  const [password, setPassword] = useState("");
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
   const [lowerCase, setLowerCase] = useState(true);
-  const [upperCase, setupperCase] = useState(false);
-  const [numbers, setNumbers] = useState(false);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(true);
   const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {
+    let charList = "";
+
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const digitChars = "0123456789";
+    const specialChars = "!@#$%^&*()_+";
+
+    if (upperCase) {
+      charList += upperCaseChars;
+    }
+    if (lowerCase) {
+      charList += lowerCaseChars;
+    }
+    if (numbers) {
+      charList += digitChars;
+    }
+    if (symbols) {
+      charList += specialChars;
+    }
+    const passwordResult = createPassword(charList, passwordLength);
+
+    setPassword(passwordResult);
+    setIsPassGenerated(true);
+  };
+
+  const createPassword = (char: string, passwordLength: number): string => {
+    let result = "";
+
+    for (let index = 0; index < passwordLength; index++) {
+      const characterIndex = Math.round(Math.random() * char.length);
+      result += char.charAt(characterIndex);
+    }
+
+    return result;
+  };
+
+  const resetPassword = () => {
+    setPassword("");
+    setIsPassGenerated(false);
+    setLowerCase(true);
+    setUpperCase(false);
+    setNumbers(false);
+    setSymbols(false);
+  };
 
   return (
     <SafeAreaView>
@@ -25,5 +71,99 @@ const App = () => {
     </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  formContainer: {
+    margin: 8,
+    padding: 8,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "600",
+    marginBottom: 15,
+  },
+  subTitle: {
+    fontSize: 26,
+    fontWeight: "600",
+    marginBottom: 2,
+  },
+  description: {
+    color: "#758283",
+    marginBottom: 8,
+  },
+  heading: {
+    fontSize: 15,
+  },
+  inputWrapper: {
+    marginBottom: 15,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  inputColumn: {
+    flexDirection: "column",
+  },
+  inputStyle: {
+    padding: 8,
+    width: "30%",
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#16213e",
+  },
+  errorText: {
+    fontSize: 12,
+    color: "#ff0d10",
+  },
+  formActions: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  primaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: "#5DA3FA",
+  },
+  primaryBtnTxt: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "700",
+  },
+  secondaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: "#CAD5E2",
+  },
+  secondaryBtnTxt: {
+    textAlign: "center",
+  },
+  card: {
+    padding: 12,
+    borderRadius: 6,
+    marginHorizontal: 12,
+  },
+  cardElevated: {
+    backgroundColor: "#ffffff",
+    elevation: 1,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: "#333",
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  generatedPassword: {
+    fontSize: 22,
+    textAlign: "center",
+    marginBottom: 12,
+    color: "#000",
+  },
+});
 
 export default App;
